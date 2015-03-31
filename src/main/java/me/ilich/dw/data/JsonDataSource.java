33e905d101d3339e869fe42a92082cac7740ce79
Dmitry I. Sokolov <Dmitry.I.Sokolov@altarix.ru>
2015-03-31 09:55:12 +0400

    maven

--- src/main/java/me/ilich/dw/data/JsonDataSource.java
+++ src/main/java/me/ilich/dw/data/JsonDataSource.java
@@ -55,7 +55,7 @@ public class JsonDataSource implements DataSource {
                     }
                 }
             }
-        } catch (IOException | ParseException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
